baeea0176149d2a9dbc556cb5148be944c65fea5
Alex Geis <siegxela@gmail.com>
2022-10-16 17:37:40 -0600

    add tagItem.js logic and CSS

---
+++ client/src/components/TagItem.js
@@ -0,0 +1,12 @@
+import styles from "./TagItem.module.css";
+
+export const TagItem = ({ status, onClick, tag }) => {
+	return (
+		<div
+			className={`${styles.tag} ${status === "active" ? styles.active : ""}`}
+			onClick={onClick}
+		>
+			{tag}
+		</div>
+	);
+};
