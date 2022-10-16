1a56da1991dc0cb21db7fbd25148c011d516cf42
Alex Geis <siegxela@gmail.com>
2022-10-16 11:16:03 -0600

    refactor tracks object in index.js to /src/assets/audio/audio.js

---
+++ client/src/assets/audio/audio.js
@@ -0,0 +1,51 @@
+import proto from "./Proto.wav";
+import sole from "./Sole.wav";
+import losingGravity from "./Losing_Gravity.wav";
+import frostwire from "./Frostwire.wav";
+import underTheSea from "./Under_the_Sea.wav";
+import flush from "./flush.wav";
+import suseok from "./Suseok.wav";
+import noninertial from "./Noninertial.wav";
+
+export const tracks = [
+	{
+		url: proto,
+		title: "Peace Pleasure - Proto",
+		tags: ["house"],
+	},
+	{
+		url: sole,
+		title: "Peace Pleasure - Sole",
+		tags: ["dnb"],
+	},
+	{
+		url: losingGravity,
+		title: "Peace Pleasure - Losing Gravity",
+		tags: ["dubstep"],
+	},
+	{
+		url: frostwire,
+		title: "Peace Pleasure - Frostwire",
+		tags: ["dubstep"],
+	},
+	{
+		url: underTheSea,
+		title: "Peace Pleasure - Under the Sea",
+		tags: ["dubstep"],
+	},
+	{
+		url: flush,
+		title: "Peace Pleasure - flush",
+		tags: ["dubstep"],
+	},
+	{
+		url: suseok,
+		title: "Peace Pleasure - Suseok",
+		tags: ["dubstep"],
+	},
+	{
+		url: noninertial,
+		title: "Peace Pleasure - Noninertial",
+		tags: ["dubstep"],
+	},
+];
--- client/src/index.js
+++ client/src/index.js
@@ -2,24 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Player } from "./App";
+import { tracks } from "./assets/audio/audio.js";
 
-const tracks = [
-	{
-		url: "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3",
-		title: "Peace Pleasure - Chords of Life",
-		tags: ["house"],
-	},
-	{
-		url: "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3",
-		title: "Peace Pleasure - Late Night Drive",
-		tags: ["dnb"],
-	},
-	{
-		url: "https://audioplayer.madza.dev/Madza-Persistence.mp3",
-		title: "Peace Pleasure - Persistence",
-		tags: ["dubstep"],
-	},
-];
+// const tracks = [
+// 	{
+// 		url: "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3",
+// 		title: "Peace Pleasure - Chords of Life",
+// 		tags: ["house"],
+// 	},
+// 	{
+// 		url: "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3",
+// 		title: "Peace Pleasure - Late Night Drive",
+// 		tags: ["dnb"],
+// 	},
+// 	{
+// 		url: "https://audioplayer.madza.dev/Madza-Persistence.mp3",
+// 		title: "Peace Pleasure - Persistence",
+// 		tags: ["dubstep"],
+// 	},
+// ];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
