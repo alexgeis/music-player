7dc0848b83b9bd97f470eab46247d53dbe7362b3
Alex Geis <siegxela@gmail.com>
2022-10-16 10:55:07 -0600

    init create-react-app, remove /src code and add index.js and index.css boilerplates

---
+++ client/src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Player } from "./App";
+
+const tracks = [
+	{
+		url: "https://audioplayer.madza.dev/Madza-Chords_of_Life.mp3",
+		title: "Peace Pleasure - Chords of Life",
+		tags: ["house"],
+	},
+	{
+		url: "https://audioplayer.madza.dev/Madza-Late_Night_Drive.mp3",
+		title: "Peace Pleasure - Late Night Drive",
+		tags: ["dnb"],
+	},
+	{
+		url: "https://audioplayer.madza.dev/Madza-Persistence.mp3",
+		title: "Peace Pleasure - Persistence",
+		tags: ["dubstep"],
+	},
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<Player trackList={tracks} />
+	</React.StrictMode>
+);
