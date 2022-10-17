4085f9f06b8860c405c9a330b2b850976d8b1497
Alex Geis <siegxela@gmail.com>
2022-10-17 08:43:30 -0600

    add Progress.js logic and CSS

---
+++ client/src/components/Progress.js
@@ -0,0 +1,25 @@
+import styles from "./Progress.module.css";
+
+export const Progress = ({ value, onChange, onMouseUp, onTouchEnd }) => {
+	return (
+		<div className={styles.container}>
+			<input
+				type="range"
+				min="1"
+				max="100"
+				step="1"
+				value={value}
+				className={styles.slider}
+				id="myRange"
+				onChange={onChange}
+				onMouseUp={onMouseUp}
+				onTouchEnd={onTouchEnd}
+				style={{
+					background: `linear-gradient(90deg, var(--progressUsed) ${Math.floor(
+						value
+					)}%, var(--progressLeft) ${Math.floor(value)}%)`,
+				}}
+			/>
+		</div>
+	);
+};
