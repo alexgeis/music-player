d40038cfff3f55ca4f28d6cdd4617984c766be8f
Alex Geis <siegxela@gmail.com>
2022-10-17 09:06:28 -0600

    add App.js main app file - import components, properly nest them for correct prop delegation, add useEffect hooks to establish initial state, add event listeners to player buttons/sliders to capture events, and add event listener functions to correspond to button actions

---
+++ client/src/components/PlaylistItem.js
@@ -0,0 +1,15 @@
+import styles from "./PlaylistItem.module.css";
+
+export const PlaylistItem = ({ status, data_key, src, title, onClick }) => {
+	return (
+		<p
+			className={`${styles.item} ${status === "active" ? styles.active : ""}`}
+			data-key={data_key}
+			src={src}
+			title={title}
+			onClick={onClick}
+		>
+			{title}
+		</p>
+	);
+};
