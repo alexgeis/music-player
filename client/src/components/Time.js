87931fbc95561bd3077ad986746f36f8861281e4
Alex Geis <siegxela@gmail.com>
2022-10-17 08:41:29 -0600

    add Title.js and Time.js logic and CSS

---
+++ client/src/components/Time.js
@@ -0,0 +1,5 @@
+import styles from "./Time.module.css";
+
+export const Time = ({ time }) => {
+	return <h1 className={styles.time}>{time}</h1>;
+};
---
+++ client/src/components/Title.js
@@ -0,0 +1,5 @@
+import styles from "./Title.module.css";
+
+export const Title = ({ title }) => {
+	return <h1 className={styles.title}>{title}</h1>;
+};
